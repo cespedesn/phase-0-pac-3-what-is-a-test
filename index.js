b12615d6e23fc00178257ae5871fbb68ec35290f
Neftali Cespedes <cesinvestmentholdings@gmail.com>
2022-04-28 13:05:38 -0400

    commit tree 259d23c891f509c05cdc0606af54d01003c4305a

--- index.js
+++ index.js
@@ -1,5 +1,52 @@
-const name = "Joe";
-const height = 74;
+const name = "Susan";
+const height = 39;
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
+
+function sayHelloToLiz() {
+    console.log("hello, Liz!");
+}
+function sayHelloToSamip() {
+    console.log("hello, Samip!");
+}
+function sayHelloToGuadalupe() {
+    console.log("hello, Guadalupe!");
+}
+sayHelloToLiz();
+sayHelloToGuadalupe();
+sayHelloToSamip();
+
+function doSomething(thing) {
+    console.log(thing);
+  }
+  
+  doSomething("anything");
+
+function sayHelloTo(firstName){
+    console.log(`hello ${firstName}!`)
+}
+sayHelloTo("Guadalupe")
+
+function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+  }
+  say("Goodbye", "Julio");
+  say("Julio", "Hello");
+
+  function say(greeting, firstName) {
+    console.log("firstName: ", firstName);
+    console.log("greeting: ", greeting);
+    console.log(`${greeting}, ${firstName}!`);
+  }
+
+  function add(x, y) {
+    return x + y;
+  }
+  console.log(add(1,2));
+
+  function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`;
+    console.log("I was called!");
+  }
+  console.log(say("Howdy", "partner"));
